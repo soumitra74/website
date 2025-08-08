9b43384b32acf6d035adb4860a39810b2f667cca
soumitraghoshiit <112617796+soumitraghoshiit@users.noreply.github.com>
2025-08-08 17:02:44 +0530

    Build fixes

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
-    basePath: isProd ? '/website' : '',
+    // basePath: isProd ? '/website' : '',
     images: {
       unoptimized: true,
     },  
