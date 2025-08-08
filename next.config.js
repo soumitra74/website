50e3fcb4a16d89a8b02bec49abbf12acc59b4076
soumitraghoshiit <112617796+soumitraghoshiit@users.noreply.github.com>
2025-08-08 11:06:25 +0530

    Changes for git pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     output: 'export',
     trailingSlash: true,
+    basePath: isProd ? '/website' : '',
+    images: {
+      unoptimized: true,
+    },  
     distDir: 'dist',
   };
-  module.exports = nextConfig;
+module.exports = nextConfig;
